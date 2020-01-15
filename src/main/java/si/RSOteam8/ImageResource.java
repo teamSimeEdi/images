86c24d09f4fada600972a9473b3c392c1333afc4
Edi Cebokli <edi.cebokli@gmail.com>
2020-01-15 11:31:47 +0100

    gcp key update

--- src/main/java/si/RSOteam8/ImageResource.java
+++ src/main/java/si/RSOteam8/ImageResource.java
@@ -184,10 +184,23 @@ public class ImageResource {
     }*/
     private static Storage storage = null;
      static {
-        storage = StorageOptions.getDefaultInstance().getService();
+        //storage = StorageOptions.getDefaultInstance().getService();
+
+
     }
     private String googleUpload(Part filePart) throws IOException {
+        String key = "{  \"type\": \"service_account\",  \"project_id\": \"testuser-260815\",  \"private_key_id\": \"44c5e2acffcc732a33aab0235693a7e27d5be418\",  \"private_key\": \"-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcimxbx56cneds\\nn0hbWywg5YOTxI17KelCfwuYBy5eeYqfXACyc6kacv2vKs87TAy1iytvXToZWU39\\nyq5aidsVib7nZf4hPj14TpJMLQWkxpWAc4NNeR/py4jmTYQYiJD9D0vGRExWM7Le\\naCOYHvIXxyUIfpMguL1I5pq8cSul2g6Y5HGeQxiJu788LXNhj61+aiIxyTNhuAeZ\\nBhOKt8zPE0zgbnLPZyj2N000n6uG0kZw/rAe62g1J6b4ENPc0JQ65ggWPZHMP9Ag\\ngD6oVY4RtiBHLRCksXJu1qTtww1ECONdYvI7mv2vAYsETfGLE1EA0E7f0+EiWPNl\\nR0GFmqW1AgMBAAECggEAEHmPvDB2lxUNrXXdqiNusbtrf52qgwWfvP2wIEiDDIAw\\nXJS7IZmVy5nE/TGpvvCAr4wor75LYYrkEl4ydaWANgTu+6rJF/MmFOdrsuCXIBbM\\nosaeS6qNeYHgpXGreUi0ag8bNkGyYSsmBMrLQ+kl5SRVjiOkeoiKbVh6vTKIE/DJ\\naLbM3jRsjYAEEdN9e4KlLgz2577zQOF1THk3tGVP2y1PdhHmPzgoeKq0b0RTBsoQ\\n4zq20gaeoUeHtv4Zuy1CZIXIgV0ut5WKvTbmajcDEbeNP4c3icxEkt/OvVuj56u7\\nRGo/1h7vZAHUy15XTmP0kcke4CAbCBQTDYkY6jDX+QKBgQD3JvdUp7tt+M9gyYNW\\nGKtVcdTB6DU0yorOSgL3mneCfO8D8py4oT4Y3XU6PiyZUfFq+7YecDwL4H9xgcNg\\nPgrVyCLNBbDNoqqGFL4KznTkneKLOcvfGiDcA7fgcaKiOT4/eMmAi9Lq7x9Ea3g8\\nEoS8fY+Sw3BSYvgBechElZocXQKBgQDkb5NGMZA90cXQdkT4Cn1tQXRYFHVxpqgV\\nYDBKqr5SorZZuGzUaSYV3vEn+MakSVhbfQsSuN8lU52vi6w6pgc4hKmvey+euMQ3\\nRf1WB6KoL31o45NPbKOF+pJWCgkb7H7L4ykZMTLjeBdBlGwZfXVkMm2oIt+2bR7e\\nkRdnVUZZOQKBgQDBFqdKcxSikS1rP2QlYnaQ9bKzn3fZ+5dAHWB+EyblQjf1zJiW\\nQhrikDniu+paCkPjQi4BT8wJt2W5xxhd23rZlFdj99sufLRetlvW039NkPAJt6jI\\nb4BWg20no3/c3337VqGOlS6+Lv0tlgzEWe9r12jQz3G0W+/IL12//2T8XQKBgHXK\\n2wR9FkXqX7io49OLhTXLZLTuh8j9CPtMyFoYRV/TK3iEUwoM1mi8t1nHPJcGgxta\\nFVWoItjajxswSLVNW7fXILCuMtYDrJpb6tBry4IyStbFUvbHrGKv3LmlHyFZB+EW\\n1+B3sCf/iu50HHHgcaIjJsxmfzlQd3SJGKpeHJJxAoGAPU7sfFirc70rpOH7wfrm\\nKAtgdSLWEtl9uV3rwIM7zvQIh0Qym/JL5jETGrDGdIg/1/yJjFPbh16hZIal/Una\\nPJkleDsNM4NVZsf4UuWMqMPSy40xxC1hSKthaPM+9N8jn5YSqfQOXxFi9xk5HkzU\\nPE4PCoUneS0JGzeyUNf+sR0=\\n-----END PRIVATE KEY-----\\n\",  \"client_email\": \"46788361512-compute@developer.gserviceaccount.com\",  \"client_id\": \"101159909331207431481\",  \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",  \"token_uri\": \"https://oauth2.googleapis.com/token\",  \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",  \"client_x509_cert_url\": \"https://www.googleapis.com/robot/v1/metadata/x509/46788361512-compute%40developer.gserviceaccount.com\"}";
 
+        try{
+            storage = StorageOptions
+                    .newBuilder()
+                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(key.getBytes())))
+                    .setProjectId("testuser-260815")
+                    .build()
+                    .getService();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
         DateTimeFormatter dtf = DateTimeFormat.forPattern("-YYYY-MM-dd-HHmmssSSS");
         DateTime dt = DateTime.now(DateTimeZone.UTC);
         String dtString = dt.toString(dtf);
